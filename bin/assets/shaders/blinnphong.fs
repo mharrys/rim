3344d4618b93469cfcb5c958be85e713c6b479f8
Mattias Harrysson <mathar@kth.se>
2015-04-03 16:03:07 +0200

    modernize blinn-phong shader

--- bin/assets/shaders/blinnphong.fs
+++ bin/assets/shaders/blinnphong.fs
@@ -1,46 +1,79 @@
 #version 130
 
-// material ambient, diffuse, specular and shininess
-uniform vec3 Ka = vec3(0.2);
-uniform vec3 Kd = vec3(0.2, 0.8, 0.2);
-uniform vec3 Ks = vec3(0.3);
-uniform float shininess = 50.0;
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    vec3 emission;
+    float shininess;
+};
 
-// light source ambient, diffuse and specular
-uniform vec3 La = vec3(0.2);
-uniform vec3 Ld = vec3(1.0);
-uniform vec3 Ls = vec3(1.0);
+struct Attenuation {
+    float constant;
+    float linear;
+    float quadratic;
+};
 
-uniform vec4 light_position;
+struct Light {
+    bool enabled;
+    vec4 position;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    Attenuation attenuation;
+};
 
-uniform float rim_width = 0.8;
+const int MAX_LIGHTS = 2;
+
+uniform Material material;
+uniform Light point_lights[MAX_LIGHTS];
 
 in vec4 position;
 in vec3 normal;
 
 out vec4 frag_color;
 
-vec3 blinn_phong_model()
+vec3 blinn_phong_model(Light light)
 {
     // direction from fragment to light source
-    vec3 s = normalize(light_position.xyz - position.xyz);
+    vec3 s = normalize(light.position.xyz - position.xyz);
     // direction vector pointing towards the viewer
     vec3 v = normalize(-position.xyz);
     // halfway vector between the viewer and light source
     vec3 h = normalize(v + s);
 
     vec3 n = normalize(normal);
-    vec3 ambient = La * Ka;
-    vec3 diffuse = Ld * Kd * max(dot(s, n), 0.0);
-    vec3 specular = max(Ls * Ks * pow(max(dot(n, h), 0.0), 4.0 * shininess), 0.0);
+    vec3 ambient = light.ambient * material.ambient;
+    vec3 diffuse = light.diffuse * material.diffuse * max(dot(s, n), 0.0);
+    vec3 specular = max(light.specular * material.specular * pow(max(dot(n, h), 0.0), 4.0 * material.shininess), 0.0);
 
     return ambient + diffuse + specular;
 }
 
+float attenuation(Light light)
+{
+    float light_distance = distance(light.position.xyz, position.xyz);
+    float denom = 0.0;
+    denom += light.attenuation.constant;
+    denom += light.attenuation.linear * light_distance;
+    denom += light.attenuation.quadratic * light_distance * light_distance;
+    return 1.0 / denom;
+}
+
 void main()
 {
-    float dist = distance(light_position.xyz, position.xyz);
-    float attenuation = 1.0 / (1.0 + (0.5 * dist + 0.03 * dist * dist));
+    vec3 color = vec3(0.0);
 
-    frag_color = vec4(blinn_phong_model() * attenuation, 1.0);
+    for (int i = 0; i < MAX_LIGHTS; i++) {
+        Light light = point_lights[i];
+
+        if (!light.enabled) {
+            continue;
+        }
+
+        color += blinn_phong_model(light) * attenuation(light);
+        color += material.emission;
+    }
+
+    frag_color = vec4(color, 1.0);
 }
