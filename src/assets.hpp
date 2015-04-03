7e4d71027f97354c89e13a807d51ef85c000fd2b
Mattias Harrysson <mathar@kth.se>
2015-04-03 15:44:24 +0200

    add assets file

---
+++ src/assets.hpp
@@ -0,0 +1,13 @@
+#ifndef ASSETS_HPP_INCLUDED
+#define ASSETS_HPP_INCLUDED
+
+#define BLINNPHONG_VS "assets/shaders/blinnphong.vs"
+#define BLINNPHONG_FS "assets/shaders/blinnphong.fs"
+#define RIMLIGHT_VS   "assets/shaders/rimlight.vs"
+#define RIMLIGHT_FS   "assets/shaders/rimlight.fs"
+#define TEXTURE_VS    "assets/shaders/texture.vs"
+#define TEXTURE_FS    "assets/shaders/texture.fs"
+
+#define SUZANNE_OBJ "assets/models/suzanne.obj"
+
+#endif
