a07ab96c78ebffb6e071862f149f743342bf3f88
Mattias Harrysson <mathar@kth.se>
2015-04-03 18:05:24 +0200

    rim light shader is now baked with a basic material

--- bin/assets/shaders/rimlight.fs
+++ bin/assets/shaders/rimlight.fs
@@ -1,21 +1,36 @@
 #version 130
 
-uniform vec3 rim_color = vec3(0.6, 0.6, 0.4);
-uniform float rim_width = 0.8;
-uniform float alpha = 1.0;
+struct Rim {
+    vec3 color;
+    float width;
+    float alpha;
+};
+
+struct Material {
+    vec3 diffuse;
+    Rim rim;
+};
+
+uniform Material material;
 
 in vec4 position;
 in vec3 normal;
 
 out vec4 frag_color;
 
+vec4 rim_light(Rim rim)
+{
+    vec3 n = normalize(normal);
+    vec3 v = normalize(-position.xyz);
+
+    float intensity = rim.width - max(dot(v, n), 0.0);
+    intensity = max(0.0, intensity); // ignore rim light if negative
+
+    return vec4(intensity * rim.color, rim.alpha);
+}
+
 void main()
 {
-    vec3 n = normal;
-    vec3 v = normalize(-position.xyz);
-    float rim_intensity = rim_width - max(dot(v, n), 0.0);
-
-    vec3 rim_light = vec3(smoothstep(0.3, 1.0, rim_intensity)) * rim_color;
-
-    frag_color = vec4(rim_light, alpha);
+    frag_color = vec4(material.diffuse, 1.0);
+    frag_color += rim_light(material.rim);
 }
